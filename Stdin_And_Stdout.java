b81219cabad7eb608a6cb3c840713c4974c10e6d
sneharani1818 <sneharani1818@gmail.com>
2023-10-08 11:03:54 +0530

    Created the stdin and out java hackerrank

---
+++ Stdin_And_Stdout.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    
+        // Complete this line
+        int s2=scan.nextInt();
+        System.out.println(s2);
+        int s=scan.nextInt();
+        System.out.println(s);
+        int s1=scan.nextInt();
+        System.out.println(s1);
+        // Complete this line
+    }
+}
+
